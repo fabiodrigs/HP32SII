fb3d1d91f7f3b266539e3bd32e85eef1603b9cba
spy <pierreyves.schuetz@gmail.com>
2017-12-25 21:45:40 +0100

    Refactoring, buttons in dictionary

---
+++ HP32SII.Calculator/Button.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HP32SII.Logic
+{
+    class Button
+    {
+        public Action<string> DefaultAction;
+        public Action<string> LeftAction;
+        public Action<string> RightAction;
+
+        public Button(Action<string> defaultAction, Action<string> leftAction, Action<string> rightAction)
+        {
+            DefaultAction = defaultAction;
+            LeftAction = leftAction;
+            RightAction = rightAction;
+        }
+    }
+}
--- HP32SII.Calculator/MainPageViewModel.cs
+++ HP32SII.Calculator/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
 using System;
+using System.Collections.Generic;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -7,10 +8,15 @@ namespace HP32SII.Logic
 {
     public class MainPageViewModel : ViewModelBase
     {
+        private Dictionary<string, Button> keyboard;
+        private Dictionary<string, Func<double, double>> dyadicOperators;
         private Calculator calculator = new Calculator();
         private Output output = new Output();
         private bool pushAtNextAppend = false;
 
+        public ICommand ButtonCommand { get; private set; }
+
+        #region TopStatus property
         private string topStatus = "";
         public string TopStatus
         {
@@ -24,7 +30,8 @@ namespace HP32SII.Logic
                 RaisePropertyChanged(nameof(TopStatus));
             }
         }
-
+        #endregion
+        #region BottomStatus property
         private string bottomStatus = "";
         public string BottomStatus
         {
@@ -38,7 +45,8 @@ namespace HP32SII.Logic
                 RaisePropertyChanged(nameof(BottomStatus));
             }
         }
-
+        #endregion
+        #region Display property
         private string display = " 0";
         public string Display
         {
@@ -52,35 +60,76 @@ namespace HP32SII.Logic
                 RaisePropertyChanged(nameof(Display));
             }
         }
-
-        public ICommand EnterCommand { get; private set; }
-        public ICommand ChangeSignCommand { get; private set; }
-        public ICommand NumericCommand { get; private set; }
-        public ICommand DotCommand { get; private set; }
-        public ICommand ClearCommand { get; private set; }
-        public ICommand DivideCommand { get; private set; }
-        public ICommand MultiplyCommand { get; private set; }
-        public ICommand SubtractCommand { get; private set; }
-        public ICommand AddCommand { get; private set; }
-        public ICommand BackspaceCommand { get; private set; }
-        public ICommand SwapCommand { get; private set; }
+        #endregion
 
         public MainPageViewModel()
         {
-            EnterCommand = new Command(HandleEnterKey);
-            ChangeSignCommand = new Command(HandleChangeSign);
-            NumericCommand = new Command<string>(HandleNumericKey);
-            DotCommand = new Command(HandleDot);
-            ClearCommand = new Command(HandleClearKey);
-            DivideCommand = new Command(HandleDivide);
-            MultiplyCommand = new Command(HandleMultiply);
-            SubtractCommand = new Command(HandleSubtract);
-            AddCommand = new Command(HandleAdd);
-            BackspaceCommand = new Command(HandleBackspace);
-            SwapCommand = new Command(HandleSwap);
+            ButtonCommand = new Command<string>(HandleButton);
+
+            keyboard = new Dictionary<string, Button>
+            {
+                // First row
+                { "SQRT", new Button(null, null, null) },
+                { "EXP", new Button(null, null, null) },
+                { "LN", new Button(null, null, null) },
+                { "POW", new Button(null, null, null) },
+                { "1/X", new Button(null, null, null) },
+                { "SUM", new Button(null, null, null) },
+                // Second row
+                { "STO", new Button(null, null, null) },
+                { "RCL", new Button(null, null, null) },
+                { "R", new Button(null, null, null) },
+                { "SIN", new Button(null, null, null) },
+                { "COS", new Button(null, null, null) },
+                { "TAN", new Button(null, null, null) },
+                // Third row
+                { "ENTER", new Button(Enter, null, null) },
+                { "SWAP", new Button(DyadicOperation, null, null) },
+                { "+/-", new Button(ChangeSign, null, null) },
+                { "E", new Button(null, null, null) },
+                { "BACK", new Button(Backspace, null, null) },
+                // Fourth row
+                { "XEQ", new Button(null, null, null) },
+                { "7", new Button(NumericKey, null, null) },
+                { "8", new Button(NumericKey, null, null) },
+                { "9", new Button(NumericKey, null, null) },
+                { "/", new Button(DyadicOperation, null, null) },
+                // Fifth row
+                { "LEFT", new Button(LeftArrow, null, null) },
+                { "4", new Button(NumericKey, null, null) },
+                { "5", new Button(NumericKey, null, null) },
+                { "6", new Button(NumericKey, null, null) },
+                { "*", new Button(DyadicOperation, null, null) },
+                // Sixth row
+                { "RIGHT", new Button(RightArrow, null, null) },
+                { "1", new Button(NumericKey, null, null) },
+                { "2", new Button(NumericKey, null, null) },
+                { "3", new Button(NumericKey, null, null) },
+                { "-", new Button(DyadicOperation, null, null) },
+                // Seventh row
+                { "C", new Button(Clear, null, null) },
+                { "0", new Button(null, null, null) },
+                { ".", new Button(HandleDot, null, null) },
+                { "R/S", new Button(null, null, null) },
+                { "+", new Button(DyadicOperation, null, null) },
+            };
+
+            dyadicOperators = new Dictionary<string, Func<double, double>>
+            {
+                { "/", calculator.Divide },
+                { "*", calculator.Multiply },
+                { "-", calculator.Subtract },
+                { "+", calculator.Add },
+                { "SWAP", calculator.Swap },
+            };
         }
 
-        private void HandleEnterKey()
+        private void HandleButton(string button)
+        {
+            keyboard[button].DefaultAction(button);
+        }
+
+        private void Enter(string button)
         {
             pushAtNextAppend = false;
             output.Freeze();
@@ -88,7 +137,17 @@ namespace HP32SII.Logic
             Display = output.ToString();
         }
 
-        private void HandleChangeSign()
+        private void LeftArrow(string button)
+        {
+            TopStatus = "  <=";
+        }
+
+        private void RightArrow(string button)
+        {
+            TopStatus = "        =>";
+        }
+
+        private void ChangeSign(string button)
         {
             output.ChangeSign();
             Display = output.ToString();
@@ -98,18 +157,18 @@ namespace HP32SII.Logic
             }
         }
 
-        private void HandleNumericKey(string digit)
+        private void NumericKey(string button)
         {
             if (pushAtNextAppend)
             {
                 calculator.Push(output.ToDouble());
                 pushAtNextAppend = false;
             }
-            output.AppendDigit(digit);
+            output.AppendDigit(button);
             Display = output.ToString();
         }
 
-        private void HandleDot()
+        private void HandleDot(string button)
         {
             if (pushAtNextAppend)
             {
@@ -120,58 +179,28 @@ namespace HP32SII.Logic
             Display = output.ToString();
         }
 
-        private void HandleClearKey()
+        private void Clear(string button)
         {
             pushAtNextAppend = false;
             output.Clear();
             Display = output.ToString();
         }
+        
+        private void DyadicOperation(string button)
+        {
+            pushAtNextAppend = false;
+            var operation = dyadicOperators[button];
+            var z = operation(output.ToDouble());
+            output.FromDouble(z);
+            Display = output.ToString();
+        }
 
-        private void HandleBackspace()
+        private void Backspace(string button)
         {
             pushAtNextAppend = false;
             output.Backspace();
             Display = output.ToString();
         }
-
-        private void HandleDivide()
-        {
-            pushAtNextAppend = false;
-            var z = calculator.Divide(output.ToDouble());
-            output.FromDouble(z);
-            Display = output.ToString();
-        }
-
-        private void HandleMultiply()
-        {
-            pushAtNextAppend = false;
-            var z = calculator.Multiply(output.ToDouble());
-            output.FromDouble(z);
-            Display = output.ToString();
-        }
-
-        private void HandleSubtract()
-        {
-            pushAtNextAppend = false;
-            var z = calculator.Subtract(output.ToDouble());
-            output.FromDouble(z);
-            Display = output.ToString();
-        }
-
-        private void HandleAdd()
-        {
-            pushAtNextAppend = false;
-            var z = calculator.Add(output.ToDouble());
-            output.FromDouble(z);
-            Display = output.ToString();
-        }
-
-        private void HandleSwap()
-        {
-            pushAtNextAppend = false;
-            var z = calculator.Swap(output.ToDouble());
-            output.FromDouble(z);
-            Display = output.ToString();
-        }
+        
     }
 }
