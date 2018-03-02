6bbd77107c5e7634bafe6473a971bda7ba893701
spy <pierreyves.schuetz@gmail.com>
2018-03-02 22:36:01 +0100

    SQLite storage implemented

--- HP32SII.Android/Database.cs
+++ HP32SII.Android/Database.cs
@@ -11,30 +11,45 @@ using Android.Views;
 using Android.Widget;
 
 using HP32SII.Logic;
+using System.IO;
+using SQLite;
 
 [assembly: Xamarin.Forms.Dependency(typeof(HP32SII.Droid.Database))]
 namespace HP32SII.Droid
 {
     class Database : IDatabase
     {
+        private const double DefaultValue = 0.0;
+        private const string Filename = "HP32SII.db";
+        private SQLiteConnection connection;
+
         public Database()
         {
-
+            string path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), Filename);
+            connection = new SQLiteConnection(path);
+            connection.CreateTable<Item>();
         }
 
         public void Store(string key, double value)
         {
-            throw new NotImplementedException();
+            var item = new Item
+            {
+                Key = key,
+                Value = value,
+            };
+
+            connection.InsertOrReplace(item);
         }
 
         public double Recall(string key)
         {
-            throw new NotImplementedException();
+            var items = connection.Query<Item>("SELECT * FROM Items WHERE Key=?", key);
+            return items.Count > 0 ? items.First<Item>().Value : DefaultValue;
         }
 
         public void ClearAll()
         {
-            throw new NotImplementedException();
+            connection.DeleteAll<Item>();
         }
     }
 }
---
+++ HP32SII.Logic/Item.cs
@@ -0,0 +1,13 @@
+﻿
+using SQLite;
+
+namespace HP32SII.Logic
+{
+    [Table("Items")]
+    public class Item
+    {
+        [PrimaryKey]
+        public string Key { get; set; }
+        public double Value { get; set; }
+    }
+}
